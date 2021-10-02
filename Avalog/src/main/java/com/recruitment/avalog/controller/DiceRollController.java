17378c0dc52bf40208368cd854f4f0d3c7d956c6
MrDanTan <91736136+MrDanTan@users.noreply.github.com>
2021-10-02 21:56:45 +0200

    Assessment(1.2) - query params for endpoint

--- Avalog/src/main/java/com/recruitment/avalog/controller/DiceRollController.java
+++ Avalog/src/main/java/com/recruitment/avalog/controller/DiceRollController.java
@@ -4,15 +4,17 @@ import com.recruitment.avalog.dto.DiceRollsSimulationResultsDTO;
 import com.recruitment.avalog.service.RollSimulatorService;
 import com.recruitment.avalog.service.converter.SimulationResultsToJsonResponseConverterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DiceRollController {
 
-    private static final int DEFAULT_NUMBER_OF_ROLLS = 100;
-    private static final int DEFAULT_NUMBER_OF_DICES = 3;
-    private static final int DEFAULT_NUMBER_OF_SIDES = 6;
+    private static final String DEFAULT_NUMBER_OF_ROLLS = "100";
+    private static final String DEFAULT_NUMBER_OF_DICES = "3";
+    private static final String DEFAULT_NUMBER_OF_SIDES = "6";
 
     private RollSimulatorService rollSimulatorService;
     private SimulationResultsToJsonResponseConverterService simulationResultsToJsonResponseConverterService;
@@ -24,11 +26,15 @@ public class DiceRollController {
         this.simulationResultsToJsonResponseConverterService = simulationResultsToJsonResponseConverterService;
     }
 
-    @GetMapping(value = "/get/dice-rolls", produces = "text/html")
-    public String defaultDiceRoll() {
-        DiceRollsSimulationResultsDTO diceRollsSimulationResultsDTO = rollSimulatorService.simulate(DEFAULT_NUMBER_OF_ROLLS,
-                                                                                                    DEFAULT_NUMBER_OF_DICES,
-                                                                                                    DEFAULT_NUMBER_OF_SIDES);
+    @GetMapping(value = "/get/dice-rolls", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String customDiceRoll(
+            @RequestParam(name = "number_of_rolls", defaultValue = DEFAULT_NUMBER_OF_ROLLS) int numberOfRolls,
+            @RequestParam(name = "number_of_dices", defaultValue = DEFAULT_NUMBER_OF_DICES) int numberOfDices,
+            @RequestParam(name = "number_of_sides", defaultValue = DEFAULT_NUMBER_OF_SIDES) int numberOfSides
+    ) {
+        DiceRollsSimulationResultsDTO diceRollsSimulationResultsDTO = rollSimulatorService.simulate(numberOfRolls,
+                                                                                                    numberOfDices,
+                                                                                                    numberOfSides);
 
         return simulationResultsToJsonResponseConverterService.convertToJson(diceRollsSimulationResultsDTO);
     }
